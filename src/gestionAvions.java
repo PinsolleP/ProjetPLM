94f9de5477d287de59c8c6c81e628f83f1db58e2
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-03 15:53:38 +0200

    Ajout méthode initavion

--- src/gestionAvions.java
+++ src/gestionAvions.java
@@ -1,2 +1,49 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class gestionAvions {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println (initavion());
+
+    }
+    public static Map<Integer, Map<String, String>> initavion() {
+        Map<Integer, Map<String, String>> avions = new HashMap<>();
+
+        Map<String, String> avion1 = new HashMap<>();
+        Map<String, String> avion2 = new HashMap<>();
+        Map<String, String> avion3 = new HashMap<>();
+        Map<String, String> avion4 = new HashMap<>();
+        Map<String, String> avion5 = new HashMap<>();
+
+
+        avion1.put("programme" , "A380");
+        avion1.put("phase actuelle" , "construction");
+        avion1.put("type" , "transport passager");
+
+        avion2.put("programme" , "A220");
+        avion2.put("phase actuelle" , "en service");
+        avion2.put("type" , "militaire");
+
+        avion3.put("programme" , "A300");
+        avion3.put("phase actuelle" , "clôturé");
+        avion3.put("type" , "avion d'affaires");
+
+        avion4.put("programme" , "A350");
+        avion4.put("phase actuelle" , "en service");
+        avion4.put("type" , "transport passager");
+
+        avion5.put("programme" , "A320");
+        avion5.put("phase actuelle" , "conception");
+        avion5.put("type" , "transport passager");
+
+        avions.put(1, avion1);
+        avions.put(2, avion2);
+        avions.put(3, avion3);
+        avions.put(4, avion4);
+        avions.put(5, avion5);
+
+        return avions;
+    }
 }
